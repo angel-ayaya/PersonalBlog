46dd1648e58a5ba8115e50dbd5ad52451b1f8ff4
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-03-12 10:55:14 -0700

    Add user routes

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import userRoutes from "./src/routes/userRoutes";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
+
+// User routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
